32b137a0448fa355024d1a7acf68b93e8b0ab244
anglczbla <angelicaizabella90@gmail.com>
2025-11-15 15:51:08 +0700

    crud news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,157 @@
+import { useState } from "react";
+
 const News = () => {
-  return <div>News</div>;
+  const [formNews, setFormNews] = useState({
+    id: "",
+    title: "",
+    body: "",
+  });
+
+  const [news, setNews] = useState([]);
+  console.log("isi news", news);
+
+  const [editNews, setEditNews] = useState({
+    id: "",
+    title: "",
+    body: "",
+  });
+  const [showToggleNews, setShowToggleNews] = useState(null);
+
+  // useEffect(() => {
+  //   const dataNews = localStorage.getItem("news");
+  //   if (dataNews) {
+  //     const data = JSON.parse(dataNews);
+  //     setNews(Array.isArray(data) ? data : []);
+  //   }
+  // }, []);
+
+  const handleChangeNews = (e) => {
+    const { name, value } = e.target;
+    setFormNews({ ...formNews, [name]: value });
+  };
+
+  const handleChangeEditNews = (e) => {
+    const { name, value } = e.target;
+    setEditNews({ ...editNews, [name]: value });
+  };
+
+  const addNews = (e) => {
+    e.preventDefault();
+
+    const newNewsList = [...news, { ...formNews }];
+    setNews(newNewsList);
+    localStorage.setItem("news", JSON.stringify(newNewsList));
+
+    setFormNews({
+      id: "",
+      title: "",
+      body: "",
+    });
+  };
+
+  const deleteNews = (id) => {
+    const deleteNewNews = news.filter((item) => item.id !== id);
+    setNews(deleteNewNews);
+    localStorage.setItem("news", JSON.stringify(deleteNewNews));
+  };
+
+  const toggleNews = (item, index) => {
+    setShowToggleNews(index);
+    setEditNews(item);
+  };
+
+  const saveEdit = (item, editNews) => {
+    const newNewsEdit = news.map((newss) =>
+      newss.id === item.id
+        ? {
+            id: editNews.id,
+            title: editNews.title,
+            body: editNews.body,
+          }
+        : newss
+    );
+    setNews(newNewsEdit);
+    localStorage.setItem("news", JSON.stringify(newNewsEdit));
+    setShowToggleNews(null);
+  };
+
+  return (
+    <div>
+      <form onSubmit={addNews}>
+        <label htmlFor="id"></label>
+        <input
+          type="text"
+          name="id"
+          value={formNews.id}
+          placeholder="Masukan id news"
+          onChange={handleChangeNews}
+        />
+        <label htmlFor="title"></label>
+        <input
+          type="text"
+          name="title"
+          value={formNews.title}
+          placeholder="Masukan title news"
+          onChange={handleChangeNews}
+        />
+        <label htmlFor="body"></label>
+        <input
+          type="text"
+          name="body"
+          value={formNews.body}
+          placeholder="Masukan body news"
+          onChange={handleChangeNews}
+        />
+        <button type="submit">Add News</button>
+      </form>
+
+      <h1>List News</h1>
+      {news.map((item, index) => (
+        <div key={item.id}>
+          <ul>
+            <li>ID: {item.id}</li>
+            <li>Title: {item.title}</li>
+            <li>Body:{item.body}</li>
+          </ul>
+          <button onClick={() => deleteNews(item.id)}>Delete news</button>
+          <button onClick={() => toggleNews(item, index)}>Edit</button>
+          {showToggleNews == index ? (
+            <div>
+              <form>
+                <label htmlFor="id">ID</label>
+                <input
+                  type="text"
+                  name="id"
+                  value={editNews.id}
+                  placeholder="Input ID Baru"
+                  onChange={handleChangeEditNews}
+                />
+                <label htmlFor="title">Title</label>
+                <input
+                  type="text"
+                  name="title"
+                  value={editNews.title}
+                  placeholder="Input Title Baru"
+                  onChange={handleChangeEditNews}
+                />
+                <label htmlFor="body">Body</label>
+                <input
+                  type="text"
+                  name="body"
+                  value={editNews.body}
+                  placeholder="Input Body Baru"
+                  onChange={handleChangeEditNews}
+                />
+              </form>
+              <button onClick={() => saveEdit(item, editNews)}>
+                Save Edit
+              </button>
+            </div>
+          ) : null}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default News;
